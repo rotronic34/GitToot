c6291b082f2cbed25099b4756a671158ae5018a6
Roeut Chak <roeut.chak@gmail.com>
2018-12-10 22:55:23 -0500

    revert

--- GitToot/ViewController.swift
+++ GitToot/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello cs193p Stanford")
-        print("hello root chak")
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
