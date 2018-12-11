ef0a5839eee5941cfe14818c06b6a892c98e592d
Roeut Chak <roeut.chak@gmail.com>
2018-12-10 22:51:43 -0500

    added a print(hello to cs193 and stanford)

--- GitToot/ViewController.swift
+++ GitToot/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hello cs193p Stanford")
         print("hello root chak")
     }
 
